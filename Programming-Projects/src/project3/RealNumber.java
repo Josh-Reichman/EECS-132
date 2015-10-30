40ae4fb41aeab053afcd93c413e01dc53eb70651
Josh Reichman <joshreichman@Joshs-MacBook-Pro.local>
2015-10-30 14:01:46 -0400

    project 3 update

--- Programming-Projects/src/project3/RealNumber.java
+++ Programming-Projects/src/project3/RealNumber.java
@@ -11,29 +11,73 @@ public class RealNumber {
 	private int precision;
 	private boolean isNegative;
 	private byte[] data;
-	
-	public RealNumber(int precision, boolean isNegative, byte[] data){
-		
+
+	public RealNumber(int precision, boolean isNegative, byte[] data) {
+		if (precision <= data.length) {
+
+		}
 	}
-	public RealNumber(int precision, String value){
-		
+
+	public RealNumber(int precision, String value) {
+
 	}
+
 	/**
 	 * @return Precision
 	 */
-	public int getPrecision(){
-		
-	}6
+	public int getPrecision() {
+		return precision;
+	}
+
 	/**
 	 * @return isNegative
 	 */
 	public boolean isNegative() {
 		return isNegative;
 	}
+
 	/**
 	 * @return data
 	 */
 	public byte[] getData() {
 		return data;
+	}
+
+	@Override
+	public String toString() {
+
+	}
+
+	public static int compare(RealNumber value1, RealNumber value2) {
+		
+	}
+
+	public int compareTo(RealNumber value) {
+
+	}
+
+	@Override
+	public boolean equals(Object o) {
+
+	}
+
+	public static RealNumber add(RealNumber value1, RealNumber value2) {
+
+	}
+
+	public static RealNumber subtract(RealNumber value1, RealNumber value2) {
+
+	}
+
+	public static RealNumber multiply(RealNumber value1, RealNumber value2) {
+
+	}
+
+	public static RealNumber divide(RealNumber value1, RealNumber value2) {
+
+	}
+
+	public static RealNumber squareRoot(RealNumber value) {
+
 	}
 }
