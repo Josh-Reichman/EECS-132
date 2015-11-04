8b9c1414ce4a5ff97e16496285d90b0df9fc53cd
Josh Reichman <reichmanjosh@gmail.com>
2015-11-04 14:07:05 -0500

    javadoc implementation

--- Programming-Projects/src/project3/RealNumber.java
+++ Programming-Projects/src/project3/RealNumber.java
@@ -5,20 +5,30 @@ package project3;
 
 /**
  * @author Josh Reichman
- *
  */
 public class RealNumber {
-	private int precision;
-	private boolean isNegative;
-	private byte[] data;
-
-	public static void main(String[] args)
-	{ 
-		RealNumber test = new RealNumber(4, false, new byte[]{1,8,9,9,8,7});
-		RealNumber test2 = new RealNumber(4, false, new byte[]{1,2,1,9,9,8,8});
-		System.out.println(RealNumber.add(test, test2));
-	}
 	/**
+	 * an integer that indicates the number of digits to the right of the decimal point
+	 */
+	private int precision;
+	/**
+	 * a boolean that determines whether the is data negative or positive
+	 */
+	private boolean isNegative;
+	/**
+	 * an array that stores all the digits of RealNumber
+	 */
+	private byte[] data;
+	/*
+ public static void main(String[] args)
+ { 
+  RealNumber test = new RealNumber(4, false, new byte[]{1,8,9,9,8,7});
+  RealNumber test2 = new RealNumber(4, false, new byte[]{1,2,1,9,9,8,8});
+  System.out.println(RealNumber.add(test, test2));
+ }
+	 */
+	/**
+	 * Primary constructor using an array as input data
 	 * @param precision
 	 * @param isNegative
 	 * @param data
@@ -39,18 +49,26 @@ public class RealNumber {
 			this.data = data;
 	}
 
+	/**
+	 * Secondary constructor using a String as input data
+	 * @param precision
+	 * @param value
+	 * @see {@literal this constructor is currently non functional}
+	 */
 	public RealNumber(int precision, String value) {
 
 	}
 
 	/**
-	 * @return Precision
+	 * Returns the number of digits to the right of RealNumber's decimal point
+	 * @return precision of the RealNumber
 	 */
 	public int getPrecision() {
 		return this.precision;
 	}
 
 	/**
+	 * Returns a boolean value dependent on the RealNumber's isNegative value
 	 * @return isNegative
 	 */
 	public boolean isNegative() {
@@ -58,25 +76,32 @@ public class RealNumber {
 	}
 
 	/**
-	 * @return data
+	 * Returns the values within RealNumber
+	 * @return data 
+	 * @see {@literal Only accessible within methods} 
 	 */
-	public byte[] getData() {
+	private byte[] getData() {
 		return this.data;
 	}
 
+	/** (non-Javadoc)
+	 * @return sb.toString
+	 * @see {@literal This method is currently not correct sometimes}
+	 */
 	@Override
 	public String toString() {
-
-		StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder(); //StringBuilder used for creating output string
 		if(this.isNegative())
 			sb.append("-");
 		if (this.getPrecision() == 0) {
+			//Loops through all data without having a decimal
 			for (int i = this.getData().length - 1; i >= 0; i--) {
 				sb.append(this.getData()[i]);
 			}
 		}
 		else if(this.getPrecision() > this.getData().length - 1){ 
 			sb.append(".");
+			//Loops through all data placing zeros where there is less data than the number of precision values needed
 			for(int i = this.getPrecision()-1; i >= 0; i--){
 				if(i > this.getData().length-1){
 					sb.append("0");
@@ -88,8 +113,9 @@ public class RealNumber {
 
 		}
 		else if(this.getPrecision() < this.getData().length - 1){ //overflow data goes past decimal point
-			boolean leadingZero = true;
-			int currentZero = 0;
+			boolean leadingZero = true; //Checks if there's a zero at the beginning of data[]
+			int currentZero = 0; //Counts number of zero's until the first positive number is read
+			//Loops through all data excluding beginning zeros in the output
 			for(int i = this.getData().length - 1;i >= 0; i--){
 				if(leadingZero){
 					if(leadingZero && ((this.getData()[i] != 0)||(this.getPrecision()==i))){
@@ -117,8 +143,9 @@ public class RealNumber {
 				}
 			}
 		}
-		else{
+		else{ //if Precision == this.getData().length
 			sb.append(".");
+			//Loops through all data placing a decimal in front
 			for (int i = this.getData().length - 2; i >= 0; i--) {
 				sb.append(this.getData()[i]);
 			}
@@ -126,14 +153,33 @@ public class RealNumber {
 
 		return sb.toString();
 	}
-	/*
- public static int compare(RealNumber value1, RealNumber value2) {
 
- }
+	/**
+	 * Returns a positive value if value1 > value2, negative value if value1 < value2, or 0 if value1 = value2
+	 * @param value1
+	 * @param value2
+	 * @return integer
+	 * @see {@literal This method is currently non functional}
+	 */
+	public static int compare(RealNumber value1, RealNumber value2) {
 
- public int compareTo(RealNumber value) {
+	}
 
- }
+	/**
+	 * Returns a positive value if the RealNumber > value, negative value if the RealNumber < value, or 0 if the RealNumber = value
+	 * @param value
+	 * @return integer
+	 * @see {@literal This method is currently non functional}
+	 */
+	public int compareTo(RealNumber value) {
+
+	}
+
+	/** 
+	 * Checks if a RealNumber is equivalent to another RealNumber
+	 * @param o
+	 * @return boolean
+	 * @throws ClassCastException
 	 */
 	@Override
 	public boolean equals(Object o) throws ClassCastException{
@@ -144,10 +190,18 @@ public class RealNumber {
 
 	}
 
+	/**
+	 * Adds two RealNumber objects together
+	 * @param value1
+	 * @param value2
+	 * @return {@literal RealNumber}
+	 * @see {@literal This method is currently not fully functional}
+	 * @see {@literal It can only get the correct length of addData}
+	 */
 	public static RealNumber add(RealNumber value1, RealNumber value2) {
-		int precision;
-		boolean isNegative = false;
-		byte[] addData;
+		int precision; // new RealNumber array precision
+		boolean isNegative = false;// new RealNumber isNegative value (currently just false)
+		byte[] addData; // new RealNumber data array
 		boolean greaterRealnumber; //true = value1. false = value2
 		if (value1.getPrecision() >=value2.getPrecision()){
 			precision = value1.getPrecision();
@@ -164,8 +218,9 @@ public class RealNumber {
 			greaterRealnumber = false;
 		}
 		System.out.println(addData.length);
-		if(greaterRealnumber){//value1 is bigger
-			for(int i1 = addData.length - 2,i2 = i1 - (i1 - value2.getData().length-1); i1 >= 0; i1--,i2--){	  
+		if(greaterRealnumber){
+			//Loops through data taking in the fact the value1 >= value2
+			for(int i1 = addData.length - 2,i2 = i1 - (i1 - value2.getData().length-1); i1 >= 0; i1--,i2--){   
 
 				if(i2>=0){
 					if((byte) (value1.getData()[i1]+ value2.getData()[i2])>9){
@@ -181,16 +236,17 @@ public class RealNumber {
 				}
 			}
 		}
-		else{//value2 is bigger
-			for(int i2 = addData.length - 2,i1 = i2 - (i2 - value1.getData().length-1); i2 >= 0; i1--,i2--){	  
+		else{
+			//Loops through data taking in the fact the value1 < value2
+			for(int i2 = addData.length - 2,i1 = i2 - (i2 - value1.getData().length-1); i2 >= 0; i1--,i2--){   
 
 				if(i2>=0){
 					//if((byte) (value1.getData()[i1]+ value2.getData()[i2])>9){
-					//	addData[i2+1]++;
-					//	addData[i2] = (byte) (value1.getData()[i1]+ value2.getData()[i2]-10);
+					// addData[i2+1]++;
+					// addData[i2] = (byte) (value1.getData()[i1]+ value2.getData()[i2]-10);
 					//} 
 					//else{
-						addData[i2] = (byte) (value1.getData()[i1]+ value2.getData()[i2]);
+					addData[i2] = (byte) (value1.getData()[i1]+ value2.getData()[i2]);
 					//}
 				}
 				else{
@@ -198,24 +254,51 @@ public class RealNumber {
 				}
 			}
 		}
-		return new RealNumber(precision,isNegative,new byte[]{1,1});
+		//  return new RealNumber(precision,isNegative,addData[]);
 	}
 
-	/*
- public static RealNumber subtract(RealNumber value1, RealNumber value2) {
 
- }
-
- public static RealNumber multiply(RealNumber value1, RealNumber value2) {
-
- }
-
- public static RealNumber divide(RealNumber value1, RealNumber value2) {
-
- }
-
- public static RealNumber squareRoot(RealNumber value) {
-
- }
+	/**
+	 * Subtracts two RealNumber objects together
+	 * @param value1
+	 * @param value2
+	 * @return RealNumber 
+	 * @see {@literal this method is currently non functional}
 	 */
+	public static RealNumber subtract(RealNumber value1, RealNumber value2) {
+
+	}
+
+	/**
+	 * Multiplies two RealNumber objects together
+	 * @param value1
+	 * @param value2
+	 * @return RealNumber 
+	 * @see {@literal this method is currently non functional}
+	 */
+	public static RealNumber multiply(RealNumber value1, RealNumber value2) {
+
+	}
+
+	/**
+	 * divides two RealNumber objects together
+	 * @param value1
+	 * @param value2
+	 * @return RealNumber 
+	 * @see {@literal this method is currently non functional}
+	 */
+	public static RealNumber divide(RealNumber value1, RealNumber value2) {
+
+	}
+
+	/**
+	 * Takes the square root of a RealNumber
+	 * @param value
+	 * @return RealNumber 
+	 * @see {@literal this method is currently non functional}
+	 */
+	public static RealNumber squareRoot(RealNumber value) {
+
+	}
+
 }
